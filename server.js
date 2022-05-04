aefca379cdf5f861875078198a60d6209e95d9ce
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-05-04 18:12:12 +0600

    server create and connected with mongo

--- server.js
+++ server.js
@@ -1,1 +1,46 @@
-console.log('hello');
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wbiyf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoryCollection = client.db('modernoFurniture').collection('inventory');
+        console.log('db connected');
+
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running MEDERNA Server');
+});
+
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
