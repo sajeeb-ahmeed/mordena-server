9590f9db4211a3c3a0cdd1430fe72266ab735153
Sajeeb Ahmed <85227514+sajeeb-ahmeed@users.noreply.github.com>
2023-01-30 00:21:29 +0600

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wbiyf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://sajeeb:ld2rQ1nUIHH3qgJS@cluster0.wbiyf.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
